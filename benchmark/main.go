79fd16d02b46caf9b32e90ec58069ee37d3e0157
George Shaw <george.e.shaw.iv@outlook.com>
2020-07-09 12:23:00 -0500

    init

---
+++ benchmark/main.go
@@ -0,0 +1,21 @@
+package benchmark
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+
+	"github.com/getoutreach/benchmarker/lib/benchmarker"
+)
+
+var counter uint32
+
+func BenchMain(f *benchmarker.Foo) {
+	res, err := http.DefaultClient.Get(f.Addr)
+	if err != nil {
+		fmt.Println("error", err)
+	}
+	atomic.AddUint32(&counter, 1)
+
+	fmt.Printf("success:\n\tcount: %d\n\tcode: %d\n---\n", counter, res.StatusCode)
+}
